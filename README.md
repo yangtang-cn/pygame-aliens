2b9fc296f1beb5bf405ef845df4f7a3530af94a2
yangtang <vimine@vip.qq.com>
2020-06-14 15:54:17 +0800

    add some detail description

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # pygame-aliens
+
+花了点时间复现下经典的《飞机大战外星人游戏》，动手回顾下Pygame
+
+主要功能模块
+
+- game_function.py
+
+类模块
+
+- alien
+- ship
+- bullet
+- scorebord
+- game_stats
+
+附带pygame的中文文档（不全），如果有未找到的函数模块说明，可以访问：[Pygame官方tutorial](https://www.pygame.org/docs/) 
+
+附上游戏截图：
+
+![image-20200614155257384](res/image-20200614155257384.png) 
--- settings.py
+++ settings.py
@@ -10,11 +10,11 @@ class Settings():
 
         # 飞船的设置
         self.ship_speed_factor = 1.5
-        self.ship_limit = 10
+        self.ship_limit = 3
 
         # 子弹的设置
         self.bullet_speed_factor = 0.1
-        self.bullet_width = 800
+        self.bullet_width = 10
         self.bullet_height = 15
         self.bullet_color = 60,60,60
         self.bullet_allowed = 10
